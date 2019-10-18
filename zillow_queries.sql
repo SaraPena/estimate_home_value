30a16ed6fbc3f35a851925bdf223d2a5c8e45dd1
SaraPena <pena.e.sara@gmail.com>
2019-10-18 10:38:50 -0500

    Delete wrangle.py, update zillow_queries.sql

--- zillow_queries.sql
+++ zillow_queries.sql
@@ -1,27 +1,44 @@
+/* Use zillow database */
 use zillow;
 
-SELECT DISTINCT(regionidzip)
-FROM properties_2016;
-
-SELECT *
-from properties_2017
-JOIN predictions_2017 using(parcelid)
-WHERE unitcnt = 1.0 and (transactiondate BETWEEN '2017-05-01' and '2017-06-30');
-
-SELECT taxamount/taxvaluedollarcnt as tax_rate, fips
-from properties_2017
-JOIN predictions_2017 using(parcelid)
-WHERE (propertylandusetypeid = 261) and (transactiondate BETWEEN '2017-05-01' and '2017-06-30') and fips = 6037;
-
+/* Find single family residential (propertylandusetypeid = 261) that were purchased between May and June of 2017 */
 SELECT *
 from properties_2017
 JOIN predictions_2017 using(parcelid)
 WHERE propertylandusetypeid = 261 and (transactiondate BETWEEN '2017-05-01' and '2017-06-30');
 
+
+-- Find the distinct values of the counties.
 SELECT DISTINCT(fips)
 from properties_2017
 JOIN predictions_2017 using(parcelid)
 WHERE propertylandusetypeid = 261 and (transactiondate BETWEEN '2017-05-01' and '2017-06-30');
+
+-- Find the tax rate distribution of fips = 6037 
+SELECT taxamount/taxvaluedollarcnt as tax_rate
+from properties_2017
+JOIN predictions_2017 using(parcelid)
+WHERE (propertylandusetypeid = 261) and (transactiondate BETWEEN '2017-05-01' and '2017-06-30') and fips = 6037;
+
+-- Find the tax rate distribution of fips = 6059 
+SELECT taxamount/taxvaluedollarcnt as tax_rate
+from properties_2017
+JOIN predictions_2017 using(parcelid)
+WHERE (propertylandusetypeid = 261) and (transactiondate BETWEEN '2017-05-01' and '2017-06-30') and fips = 6059;
+
+
+-- Find the tax rate distribution of fips = 6111 
+SELECT taxamount/taxvaluedollarcnt as tax_rate
+from properties_2017
+JOIN predictions_2017 using(parcelid)
+WHERE (propertylandusetypeid = 261) and (transactiondate BETWEEN '2017-05-01' and '2017-06-30') and fips = 6111;
+
+-- Fields to bring into python df:
+
+SELECT parcelid, bathroomcnt, bedroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, taxamount
+from properties_2017
+JOIN predictions_2017 using(parcelid)
+WHERE (propertylandusetypeid = 261) and (transactiondate BETWEEN '2017-05-01' and '2017-06-30')
 
 
 
